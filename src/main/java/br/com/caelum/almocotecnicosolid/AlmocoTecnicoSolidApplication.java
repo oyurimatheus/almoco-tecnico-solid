218dde3fd2e17c4dc9fc3d3780bd5368c9088f06
Yuri Matheus <yurimatheusb@hotmail.com>
2018-09-25 17:06:50 -0300

    first commit

---
+++ src/main/java/br/com/caelum/almocotecnicosolid/AlmocoTecnicoSolidApplication.java
@@ -0,0 +1,12 @@
+package br.com.caelum.almocotecnicosolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmocoTecnicoSolidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmocoTecnicoSolidApplication.class, args);
+	}
+}
